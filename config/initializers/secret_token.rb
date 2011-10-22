91369fb805f37d68b616c9945ccf0add81dea75d
Mitch Kett <mitchkett@gmail.com>
2011-10-21 20:04:39 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsnipNew::Application.config.secret_token = '9e7e6d3295ca71ea19809b51b68785d020911eaaffebeca3059d392b3793f5dffcc91633bd7f7528d594e4585e448071183d41ca3bba492a46a923b5a5fc2f88'
