76116c61b36008eb6e68c0cbfac7822c1301215e
Matt Davis <matt@mattsonlyattack.net>
2011-10-22 19:18:30 -0400

    only send if email was provided

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -54,7 +54,7 @@ class AppointmentsController < ApplicationController
   def create
     @appointment = Appointment.new(params[:appointment])
     if @appointment.save
-      AppointmentMailer.appointment_submitted(@appointment).deliver
+      unless @appointment.email.blank? AppointmentMailer.appointment_submitted(@appointment).deliver
       render :layout => 'application'
     else
       render :action => :new, :layout => 'application'
