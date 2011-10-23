9b6de6fc46d5232a5e5e7a5c7542f0109027659c
Matt Davis <matt@mattsonlyattack.net>
2011-10-22 23:08:31 -0400

    more action on my mailer

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -100,6 +100,7 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
     if @appointment.update_attributes(:status => "APPROVED")
       flash[:notice] = "Appointment #{@appointment.id.to_s} with #{@appointment.client_full_name} has been approved."
+      AppointmentMailer.appointment_accepted(@appointment).deliver unless @appointment.email.blank?
       redirect_to appointments_path
     end
   end
@@ -108,6 +109,7 @@ class AppointmentsController < ApplicationController
     @appointment = Appointment.find(params[:id])
     if @appointment.update_attributes(:status => "DENIED")
       flash[:notice] = "Appointment #{@appointment.id.to_s} with #{@appointment.client_full_name} has been denied."
+      AppointmentMailer.appointment_denied(@appointment).deliver unless @appointment.email.blank?
       redirect_to appointments_path
     end
   end
