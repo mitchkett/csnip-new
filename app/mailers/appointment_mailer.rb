8617902911ae93fbf7a7c861bb36332448913fcb
Matt Davis <matt@mattsonlyattack.net>
2011-10-22 22:57:36 -0400

    stub some new mailers

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -6,4 +6,15 @@ class AppointmentMailer < ActionMailer::Base
     #@url = link_to(appointment)
     mail(:to => appointment.email, :subject => 'Your C-SNIP appointment request')
   end
+  
+  def appointment_accepted(appointment)
+    @appointment = appointment
+    mail(:to => appointment.email, :subject => 'Your C-SNIP appointment request is approved')
+  end
+  
+  def appointment_denied(appointment)
+    @appointment = appointment
+    mail(:to => appointment.email, :subject => 'Your C-SNIP appointment request')
+  end
+  
 end
