ae04848b10cfe57590c53df4ddda8e4820e5994f
Mitch Kett <mitchkett@gmail.com>
2011-10-22 22:53:17 -0400

    filtering

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -13,6 +13,60 @@ class Appointment < ActiveRecord::Base
   attr_accessor :acquired_from_type, :acquired_from_other_description
   attr_reader :age
   
+  class << self
+    def by_last_name(last_name)
+      where("last_name LIKE '%#{last_name}%'")
+    end
+    
+    def by_county(county)
+      where("county LIKE '%#{county}%'")
+    end
+    
+    def by_zip(postal_code)
+      where(:postal_code => postal_code)
+    end
+    
+    def by_pet_type(pet_type)
+      where(:pet_type => pet_type)
+    end
+    
+    def from_date(from)
+      where("created_at >= ?", from)
+    end
+    
+    def to_date(to)
+      where(["created_at <= ?", to])
+    end
+    
+    def older
+      where("date_of_birth <= ?", Time.now - 6.months)
+    end
+    
+    def younger
+      where("date_of_birth > ?", Time.now - 6.months)
+    end
+    
+    def vaccinated
+      where("pet_vaccinated LIKE '%Yes%'")
+    end
+    
+    def not_vaccinated
+      where("pet_vaccinated LIKE '%No %'")
+    end
+    
+    def seen_vet
+      where(:has_pet_seen_vet => true)
+    end
+    
+    def not_seen_vet
+      where(:has_pet_seen_vet => false)
+    end
+    
+    def by_status(status)
+      where(:status => status)
+    end
+  end  
+  
   def set_acquired_from
     if acquired_from_type == 'other'
       self.acquired_from = "other: #{acquired_from_other_description}"
